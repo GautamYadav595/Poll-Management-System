ab39cd21a92d30e7f016ac52184db8d18825bb48
yrahull595 <114355514+yrahull595@users.noreply.github.com>
2025-02-17 14:50:25 +0530

    signup/login implemented successfully

---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center ">
+  <UForm :schema="schema"  class="space-y-4 w-full max-w-md p-6 bg-white rounded-lg shadow-lg" @submit="handleLogin">
+    <h1 class="text-3xl font-semibold text-center mb-6">Login</h1>
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="email"  required/>
+    </UFormGroup>
+
+    <UFormGroup label="Password" name="password" >
+      <UInput v-model="password" type="password" required/>
+    </UFormGroup>
+
+    <UButton type="submit">
+      Submit
+    </UButton>
+    <div class="text-center mt-4">
+  <p class="text-sm">
+    Don't have an account?
+    <NuxtLink to="/Signup" class="text-blue-600">Sign up</NuxtLink>
+  </p>
+</div>
+  </UForm>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useAuth } from '~/composables/useAuth';
+
+const email = ref('');
+const password = ref('');
+const { login } = useAuth();
+
+const handleLogin = async () => {
+  try {
+    await login(email.value, password.value);
+    alert('Login successful');
+  } catch (error) {
+    alert(error.message);
+  }
+};
+</script>
+
+
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,12 +1,19 @@
-// Import the functions you need from the SDKs you need
+import { defineNuxtPlugin } from "#app";
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCc356c8coQgt1sSyR9lQLpiFYMr9n1Liw",
   authDomain: "poll-management-system-7f756.firebaseapp.com",
   projectId: "poll-management-system-7f756",
-  storageBucket: "poll-management-system-7f756.firebasestorage.app",
+  storageBucket: "poll-management-system-7f756.appspot.com", 
   messagingSenderId: "438555198200",
   appId: "1:438555198200:web:dc0e59104011671ff9310d"
 };
-const app = initializeApp(firebaseConfig);
+
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  nuxtApp.provide("auth", auth);
+});
