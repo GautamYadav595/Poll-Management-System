db21e16cff64c5de975e39d585ba16f80c513495
yrahull595 <114355514+yrahull595@users.noreply.github.com>
2025-02-17 12:21:48 +0530

    Firebase setup

---
+++ plugins/firebase.ts
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCc356c8coQgt1sSyR9lQLpiFYMr9n1Liw",
+  authDomain: "poll-management-system-7f756.firebaseapp.com",
+  projectId: "poll-management-system-7f756",
+  storageBucket: "poll-management-system-7f756.firebasestorage.app",
+  messagingSenderId: "438555198200",
+  appId: "1:438555198200:web:dc0e59104011671ff9310d"
+};
+const app = initializeApp(firebaseConfig);
